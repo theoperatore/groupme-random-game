2c80710fb19c09c3a89f78b651cd28db674a381b
Al Petersen <al@spotify.com>
2017-08-16 17:41:54 -0400

    support more game platforms; better date formatting

--- src/server.js
+++ src/server.js
@@ -16,10 +16,24 @@ const PLATFORMS = [
   'Game Gear',
   'Super Nintendo Entertainment System',
   'Nintendo Entertainment System',
+  'Game Boy',
+  'Game Boy Color',
+  'Game Boy Advance',
+  'Nintendo 64',
+  'Dreamcast',
+  'PlayStation',
+  'Genesis',
 ];
 
+const formatDate = date => new Intl.DateTimeFormat('en', {
+  timeZone: 'UTC',
+  month: 'short',
+  year: 'numeric',
+  day: 'numeric',
+}).format(new Date(date));
+
 const mapGameToPost = platform => ({ image, name, deck, description, original_release_date, site_detail_url }) => ({
-  text: `# ${name}\n# ${platform}\n# ${original_release_date}\n\n${deck}\n\n${site_detail_url}\n\n--GOTD`,
+  text: `# ${name}\n# ${platform}\n# ${formatDate(original_release_date)}\n\n${deck}\n\n${site_detail_url}\n\n--GOTD`,
   image: `${image.super_url || image.screen_url || image.medium_url || image.small_url || image.thumb_url || image.icon_url || image.tiny_url}`,
 });
 
@@ -48,6 +62,7 @@ const sendGame = platform =>
 // which should be somewhere around 8:30am central time.
 cron.schedule('30 13 * * *', () => {
   const platform = getRandom(PLATFORMS);
+  console.log('cron sending game', platform);
   sendGame(platform)
     .then(() => `did it, ${new Date().toLocaleString()}`)
     .then(console.log)
@@ -64,8 +79,7 @@ app.use(bodyParser.json());
 app.post('/random', (req, res) => {
   const { text } = req.body;
 
-  console.log(text);
-  if (!text.match('#random')) return res.sendStatus(200);
+  if (!text || !text.match('#random')) return res.sendStatus(200);
 
   const platform = getRandom(PLATFORMS);
   console.log('sending random game from post', platform);
