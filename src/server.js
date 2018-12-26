136ed6fed90a685994098bacc5e962a0fbde42ac
theoperatore <theoperatore@gmail.com>
2018-12-26 16:43:02 -0500

    can post to a production and dev based on which group messages are sent from

--- src/server.js
+++ src/server.js
@@ -16,6 +16,15 @@ import PLATFORMS from './giantBombPlatform-corpus';
 
 dotenv.config();
 
+const PROD_GROUP_ID = '13613300';
+const DEV_GROUP_ID = '17602864';
+
+const groupsToBotIds = {
+  [PROD_GROUP_ID]: process.env.PROD_BOT_ID,
+  [DEV_GROUP_ID]: process.env.DEV_BOT_ID,
+};
+
+
 // retired for the full list of platforms
 // const PLATFORMS = [
 //   'Game Gear',
@@ -65,12 +74,12 @@ const mapResultsToPost = text => ({
   image: null,
 });
 
-const postToGroupme = ({ text, image }) =>
+const postToGroupme = botId => ({ text, image }) =>
   fetch(`https://api.groupme.com/v3/bots/post?token=${process.env.GM_TOKEN}`, {
     method: 'POST',
     headers: new Headers().append('Content-Type', 'application/json'),
     body: JSON.stringify({
-      bot_id: process.env.BOT_ID,
+      bot_id: botId,
       text,
       picture_url: image,
     }),
@@ -78,7 +87,7 @@ const postToGroupme = ({ text, image }) =>
 
 const getRandom = itms => itms[Math.floor(Math.random() * (itms.length - 1))];
 
-const sendGame = platform => {
+const sendGame = (botId, platform) => {
   let gameName = '';
   return getRandomGame(process.env.GB_TOKEN, platform.id)
     .then(game => {
@@ -86,19 +95,19 @@ const sendGame = platform => {
       return game;
     })
     .then(mapGameToPost(platform.name))
-    .then(postToGroupme)
+    .then(postToGroupme(botId))
     .then(response => ({ response, gameName }));
 };
 
-const sendCompendiumEntry = rawText =>
+const sendCompendiumEntry = (botId, rawText) =>
   searchCompendium(rawText)
     .then(mapResultsToPost)
-    .then(postToGroupme);
+    .then(postToGroupme(botId));
 
-const sendWhoami = () =>
+const sendWhoami = botId =>
   whoami()
     .then(mapResultsToPost)
-    .then(postToGroupme);
+    .then(postToGroupme(botId));
 
 const generateId = () => {
   const str = 'abcdefghijklmnopqrstuvwxyz';
@@ -129,35 +138,43 @@ app.use(cors());
 app.use(compression());
 app.use(bodyParser.json());
 
-app.get('/health_check', (req, res) => res.sendStatus(204));
+app.get('/health_check', (req, res) => {
+  console.log(`[log] - ${new Date().toLocaleString()} health_check`);
+  client.get('alorg://dnd-monster-api/health_check').then(({ headers }) => {
+    if (headers[':status'] === 204) return res.sendStatus(204);
+    return res.sendStatus(headers[':status']);
+  });
+});
 
 app.post('/random', (req, res) => {
-  const { text, sender_type } = req.body;
+  const { text, sender_type, group_id } = req.body;
   const isBotMessage = sender_type === 'bot';
+  const botId = groupsToBotIds[group_id];
+console.log(group_id, botId);
 
-  if (!text || isBotMessage) return res.sendStatus(200);
+  if (!text || isBotMessage || !botId) return res.sendStatus(200);
 
   const id = generateId();
   // this goes first to ensure help text doesn't trigger
   // other commands
   if (text.match('#help')) {
-    postToGroupme({ text: helpText });
+    postToGroupme(botId)({ text: helpText });
   } else if (text.match('#whoami')) {
     console.log(`[log] ${id} generating backstory...`);
-    sendWhoami()
+    sendWhoami(botId)
       .then(() => `[log] ${id} ${new Date().toLocaleString()}`)
       .then(console.log)
       .catch(err => console.log(`[error] ${id}`, err));
   } else if (text.match('#dnd')) {
     console.log(`[log] ${id} searching compendium:`, text);
-    sendCompendiumEntry(text)
+    sendCompendiumEntry(botId, text)
       .then(() => `[log] ${id} ${new Date().toLocaleString()}`)
       .then(console.log)
       .catch(err => console.log(`[error] ${id}`, err));
   } else if (text.match('#random')) {
     const platform = getRandom(PLATFORMS);
     console.log(`[log] ${id} post sending random game`, platform.name);
-    sendGame(platform)
+    sendGame(botId, platform)
       .then(
         ({ gameName }) =>
           `[log] ${id} ${new Date().toLocaleString()} ${gameName}`
@@ -167,14 +184,14 @@ app.post('/random', (req, res) => {
   } else if (text.match('#monster')) {
     client.get('alorg://dnd-monster-api/random').then(response => {
       const monster = JSON.parse(response.payload);
-      postToGroupme({
+      postToGroupme(botId)({
         text: `# ${monster.name}\n# ${monster.race}\n# ${monster.abilities.map(a => a.name).join(', ')}`,
         image: monster.image
       });
     })
     .catch(error => {
       console.error(error);
-      postToGroupme({ text: `Failure: ${error.message}` });
+      postToGroupme(botId)({ text: `Failure: ${error.message}` });
     })
   }
 
@@ -183,5 +200,5 @@ app.post('/random', (req, res) => {
 
 const PORT = process.env.PORT || 9966;
 app.listen(PORT, () => {
-  console.log(new Date(), 'server listening on', PORT);
+  console.log(new Date().toLocaleString(), 'server listening on', PORT);
 });
