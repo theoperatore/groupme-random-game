7569dc35979cb2e6117cf43a7a1077088eaed2f3
Alex Petersen <theoperatore@gmail.com>
2019-04-25 09:19:08 -0400

    deploy scripts

--- scripts/build-deploy.sh
+++ scripts/build-deploy.sh
@@ -3,10 +3,8 @@
 tsc
 
 cp .env build/.env
-cp .npmrc build/.npmrc
-cp .yarnrc build/.yarnrc
-cp yarn.lock build/yarn.lock
+cp package-lock.json build/package-lock.json
 cp package.json build/package.json
 
-rsync -avzhP build pi@groupme.local:~/groupme-random-game
-# ssh pi@groupme.local cd groupme-random-game && npm run stop && npm install && npm run start
+rsync -avzhP build/* pi@groupme.local:~/groupme-random-game
+ssh pi@groupme.local "cd groupme-random-game && NODE_ENV=production npm -q install && NODE_ENV=production npm run start"
